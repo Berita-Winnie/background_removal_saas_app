94c844d181cda35e3e86ed4bd49f0cf4c6937dc1
Berita-Winnie <beritawinnie@gmail.com>
2026-01-23 21:05:00 +0300

    Intergrated Clerk for user authentication

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+VITE_CLERK_PUBLISHABLE_KEY =
+  pk_test_YWN0dWFsLWVtdS02Ni5jbGVyay5hY2NvdW50cy5kZXYk
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
-    </StrictMode>
+    </ClerkProvider>
   </BrowserRouter>,
 )
