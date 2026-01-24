50c4a529508077b37762fc7688eda3304d3b783d
Berita-Winnie <beritawinnie@gmail.com>
2026-01-24 23:04:29 +0300

    Used clerk for background removal popo-up window

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,8 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
 
-VITE_CLERK_PUBLISHABLE_KEY =
-  pk_test_YWN0dWFsLWVtdS02Ni5jbGVyay5hY2NvdW50cy5kZXYk
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
